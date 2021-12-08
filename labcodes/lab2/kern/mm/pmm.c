062265e2161759dcf0872cc5b2f5c6e92d1f1688
honndaj <lizeqing0@gmail.com>
2021-12-08 21:30:38 +0800

    fixs bug in page_move

--- labcodes/lab2/kern/mm/pmm.c
+++ labcodes/lab2/kern/mm/pmm.c
@@ -422,7 +422,7 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
 #endif
     if (*ptep & PTE_P) {
         struct Page *page = pte2page(*ptep);
-        if ((page->ref--) == 0) {
+        if ((--page->ref) == 0) {
             free_page(page);
         }
         *ptep = 0;
